6f0bcedbc94ade16e0a16818d66b92d78ece1b1e
loej <joel.martinez@rutgers.edu>
2020-07-05 02:03:29 -0400

    Added linkFunction to page.js. Started implementing functionality to start page.

--- src/page.js
+++ src/page.js
@@ -1,1 +1,7 @@
 /* Welcome to ~/Jarvis!. A Simple startup page for chrome. The html/css/js files will be commented thoroughly. */
+
+function linkFunction() {
+    var search = 'dev'
+    var result = search.link('https://www.youtube.com/')
+    document.getElementById('dev').innerHTML = result
+}
