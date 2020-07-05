0033569c359327e6b771c6114a6e8459533c7feb
loej <joel.martinez@rutgers.edu>
2020-07-05 02:08:46 -0400

    Renamed js file and added comments.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 /* Welcome to ~/Jarvis!. A Simple startup page for chrome. The html/css/js files will be commented thoroughly. */
 
+/* Allows .dev to link to youtube (for now). */
 function linkFunction() {
     var search = 'dev'
     var result = search.link('https://www.youtube.com/')
