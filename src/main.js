b976cd707aac0fafa579e65144e5d82e2def895c
loej <joel.martinez@rutgers.edu>
2020-07-10 03:20:46 -0400

    Implemented animations and timers. Started adding tree structure.

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 /* Welcome to ~/Jarvis!. A Simple startup page for chrome. The html/css/js files will be commented thoroughly. */
 
-/* Allows .dev to link to youtube (for now). */
+/* Allows .dev to link to Github  */
 function linkFunction() {
     let search = 'dev'
     document.getElementById('dev').innerHTML = search.link('https://github.com/loej')
 }
 
+/* Function that displays text after 4 seconds.*/
+
 function showTree() {
-    document.getElementById('treeText').style.display = 'inline'
+    /* Value is inline text by default. */
+
+    document.getElementById('treeText').style.display = ''
 }
 
-setTimeout(showTree,4100)
+setTimeout(showTree, 4100)
