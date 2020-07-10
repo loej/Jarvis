dadc625d7fea29459d8363302af337aeaedc2577
loej <joel.martinez@rutgers.edu>
2020-07-10 02:38:08 -0400

    Implemented function to setTime to display the tree text.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 /* Allows .dev to link to youtube (for now). */
 function linkFunction() {
     let search = 'dev'
-    let result = search.link('https://www.youtube.com/')
-    document.getElementById('dev').innerHTML = result
+    document.getElementById('dev').innerHTML = search.link('https://github.com/loej')
 }
+
+function showTree() {
+    document.getElementById('treeText').style.display = 'inline'
+}
+
+setTimeout(showTree,4100)
