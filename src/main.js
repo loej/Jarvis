72e72602a8ed016645b5843b3fe1a001e952b72c
loej <jfm203@scarletmail.rutgers.edu>
2020-07-21 19:53:52 -0400

    Added tree structure.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 /* Welcome to ~/Jarvis!. A Simple startup page for chrome. The html/css/js files will be commented thoroughly. */
 
 /* Allows .dev to link to Github  */
-function linkFunction() {
-    let search = 'dev'
-    document.getElementById('dev').innerHTML = search.link('https://github.com/loej')
-}
+// function linkFunction() {
+//     let search = 'dev'
+//     document.getElementById('dev').innerHTML = search.link('https://github.com/loej')
+// }
 
 /* Function that displays text after 4 seconds.*/
 
